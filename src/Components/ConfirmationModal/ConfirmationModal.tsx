99cd6c199834080952f8c6f6caa9242efd9c5604
Sandhya <sandhya.k@tringapps.net>
2024-06-18 14:20:26 +0530

    npm-fix

--- src/Components/ConfirmationModal/ConfirmationModal.tsx
+++ src/Components/ConfirmationModal/ConfirmationModal.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import './ConfirmationModal.css'; // Import custom CSS for additional styling
-
+import './ConfirmationModal.css'; 
 interface ConfirmationModalProps {
   show: boolean;
   onHide: () => void;
@@ -17,18 +15,18 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({ show, onHide, onC
   return (
     <Modal show={show} onHide={onHide} centered>
       <Modal.Header closeButton>
-        <Modal.Title>{title}</Modal.Title>
+        <Modal.Title >{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         {message}
       </Modal.Body>
       <Modal.Footer className="modal-footer-custom">
-        <Button variant="success" onClick={onHide}>
+        <button className="success" onClick={onHide}>
           {cancelLabel}
-        </Button>
-        <Button variant="primary" onClick={onConfirm}>
+        </button>
+        <button className="primary" onClick={onConfirm}>
           {confirmLabel}
-        </Button>
+        </button>
       </Modal.Footer>
     </Modal>
   );
