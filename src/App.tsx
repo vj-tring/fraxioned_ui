8d12dd65a61067dd83cb8b9bc191c047c0164c33
Dhaya1599 <129719964+Dhaya1599@users.noreply.github.com>
2024-07-26 14:59:14 +0530

    prettier issue

--- src/App.tsx
+++ src/App.tsx
@@ -34,32 +34,29 @@ const PrivateRoute: FC<PrivateRouteProps> = ({ element: Element }) => {
 }
 
 const App: FC = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/login" />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Signup />} />
-                <Route path="/change" element={<Change />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/booking" element={<Booking />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route
-                    path="/ResponsiveAppBar"
-                    element={<ResponsiveAppBar />}
-                />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/menu" element={<AccountMenu />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/date" element={<Date />} />
-                <Route path="/carosel" element={<Carosuel />} />
-                <Route
-                    path="/dashboard"
-                    element={<PrivateRoute element={Dashboard} />}
-                />
-                {/* Use PrivateRoute for all other routes that need to be protected */}
-                {/* <Route path="/*" element={<PrivateRoute element={Dashboard} />} /> */}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/change" element={<Change />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/ResponsiveAppBar" element={<ResponsiveAppBar />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/menu" element={<AccountMenu />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/date" element={<Date />} />
+        <Route path="/carosel" element={<Carosuel />} />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={Dashboard} />}
+        />
+        {/* Use PrivateRoute for all other routes that need to be protected */}
+        {/* <Route path="/*" element={<PrivateRoute element={Dashboard} />} /> */}
 
         <Route path="/multiselect" element={<MultipleSelect />} />
         <Route path="/basicselect" element={<BasicSelect />} />
