f4b7451d06d1d69f547fc193bdfb3d0e81bbd8c9
DharshanRam Kumar K <dharshanramk@gmail.com>
2024-08-09 14:00:53 +0530

    reset password E2E test for Admin

---
+++ cypress/e2e/Admin/resetpassword.cy.ts
@@ -0,0 +1,55 @@
+describe("Reset Password", () => {
+    const login = () => {
+        cy.visit("/login")
+            .get("input[placeholder='Email']").type("dharshanramk@gmail.com")
+            .get("input[placeholder='Password']").type("Admin@12")
+            .get("button").contains("Sign in").click()
+            .get(".MuiBox-root.css-1hg4z9e").click()
+            .get("div[id='account-menu'] li:nth-child(1)").click();
+    }
+    describe("Unsuccessful Reset Password", () => {
+        it("Empty Old Password", () => {
+            login();
+            cy.get("button[type='submit']").click()
+                .get(".ResetPassword_errorMessage__W8ojI").contains("Please enter your old password");
+        })
+        it("Empty New Password", () => {
+            login();
+            cy.get("#oldPassword").type("Password")
+                .get("button[type='submit']").click()
+                .get(".ResetPassword_errorMessage__W8ojI").contains("Please enter a new password");
+        })
+        it("Empty Confirm New Password", () => {
+            login();
+            cy.get("#oldPassword").type("Password")
+                .get("#newPassword").type("Password")
+                .get("button[type='submit']").click()
+                .get(".ResetPassword_errorMessage__W8ojI").contains("Please confirm your new password");
+        })
+        it("Old Password is incorrect", () => {
+            login();
+            cy.get("#oldPassword").type("Admin")
+                .get("#newPassword").type("Admin@12")
+                .get("#confirmPassword").type("Admin@12")
+                .get("button[type='submit']").click()
+                .get("div[role='alert']").contains("The provided old password is incorrect");
+        })
+        it("Password does not match", () => {
+            login();
+            cy.get("#oldPassword").type("Admin@12")
+                .get("#newPassword").type("Admin@1")
+                .get("#confirmPassword").type("Admin@12")
+                .get("button[type='submit']").click()
+                .get(".ResetPassword_errorMessage__W8ojI").contains("New passwords do not match");
+        })
+    })
+    describe("Successful Reset Password", () => {
+        it("Reset password with valid credentials", () => {
+            login();
+            cy.get("#oldPassword").type("Admin@12")
+                .get("#newPassword").type("Admin@12")
+                .get("#confirmPassword").type("Admin@12")
+                .get("button[type='submit']").click();
+        })
+    })
+})
