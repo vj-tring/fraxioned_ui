e73215dab283b318989c6fa5341c4554e3b96c43
Siva-bharathiP <136304492+Siva-bharathiP@users.noreply.github.com>
2024-08-16 16:52:49 +0530

    Select Property

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./card.css";
 import { FaPlus } from "react-icons/fa";
 import FraxImg from "../../assets/images/bookings-page-banner.jpg"
@@ -44,14 +44,20 @@ const Card: React.FC<CardProps> = ({
 
       {showPlusIcon && (
         <div className="plus-icon-container">
+                    <a href="https://www.fraxioned.com/" target="_blank">
+
         <div className="image-container ">
           <img
             src={FraxImg}
             className={`card-img-top ${showPlusIcon ? 'blur-effect' : ''}`}
             alt={title}
+            
+
           />
+          
                   {showPlusIcon && <FaPlus className="plus-icon" />}
                   </div>
+                  </a>
           <div className="card-body ">
             <h4 className="card-title">Adventure Awaits...</h4>
             <span className="card-text">Discoverd your next Fraxioned home at fraxioned.com</span>
