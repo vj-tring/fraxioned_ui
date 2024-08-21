8c95fd5d28ef1d5ed3a7155f8d506f26ebc7f063
DharshanRam Kumar K <dharshanramk@gmail.com>
2024-08-21 12:33:39 +0530

    update test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:4173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
