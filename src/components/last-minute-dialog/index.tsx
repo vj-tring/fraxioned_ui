48e3e19f86f03555e808a1e361b103eeabbac963
Sandhya <sandhya.k@tringapps.net>
2024-10-07 12:59:35 +0530

    UI fixes

--- src/components/last-minute-dialog/index.tsx
+++ src/components/last-minute-dialog/index.tsx
@@ -11,18 +11,18 @@ interface LastMinuteBookingDialogProps {
 const LastMinuteBookingDialog: React.FC<LastMinuteBookingDialogProps> = ({ isOpen, onClose, onChoice }) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="sm:max-w-[425px] rounded">
+      <DialogContent className="sm:max-w-[425px] pl-5 rounded">
         <DialogHeader>
-          <DialogTitle className="text-lg font-semibold mb-2">Last-Minute Booking</DialogTitle>
+          <DialogTitle className="text-lg font-semibold text-left mb-2">Last-Minute Booking</DialogTitle>
           <DialogDescription className="text-sm text-gray-500 text-left">
-          This is a last-minute booking. Would you prefer to proceed with this last-minute reservation, or would you like to opt for a regular booking instead?
+            This is a last-minute booking. Would you like to proceed with a last-minute booking or a regular booking?
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="sm:justify-start">
           <Button 
             variant="default" 
             onClick={() => onChoice('lastMinute')}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            className="bg-orange-300 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded"
           >
             Last-Minute Booking
           </Button>
