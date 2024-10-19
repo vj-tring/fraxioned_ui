ca4eab95bfa66736ee138c8adff06ac56f4d5bf8
DINESHKUMAR K V <dineshkumar.kv@tringapps.com>
2024-10-19 14:08:56 +0530

    Api changed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://192.168.1.77:3008/api/v1";
+export const BACKEND_URL = "http://192.168.1.47:3008/api/v1";
 export const ENCRYPTION_KEY = "fraxioned-owner-creds-key";
 export const publicRoutes = ["/login", "/forgotPassword", "/recover"];
 
