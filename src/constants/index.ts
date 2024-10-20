7b71c191d9dec99bfbdd4b95395642ca25b63a43
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-10-20 18:29:01 +0530

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-// export const BACKEND_URL = "http://192.168.1.47:3008/api/v1";
-export const BACKEND_URL = "http://localhost:3008/api/v1"
+export const BACKEND_URL = "http://192.168.1.47:3008/api/v1";
+// export const BACKEND_URL = "http://localhost:3008/api/v1"
 export const ENCRYPTION_KEY = "fraxioned-owner-creds-key";
 export const publicRoutes = ["/login", "/forgotPassword", "/recover"];
